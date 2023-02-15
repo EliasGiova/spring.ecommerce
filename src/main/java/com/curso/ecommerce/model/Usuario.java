1ccaa313485a9a36a84b971d7cd8f296050b2126
EliasGiova <eliasgiooovanella@gmail.com>
2023-02-15 15:36:06 -0300

    Mapear y Relacionar

--- src/main/java/com/curso/ecommerce/model/Usuario.java
+++ src/main/java/com/curso/ecommerce/model/Usuario.java
@@ -1,9 +1,22 @@
 
 package com.curso.ecommerce.model;
 
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
+    
     private String nombre;
     private String username;
     private String email;
@@ -11,11 +24,17 @@ public class Usuario {
     private String telefono;
     private String tipo;
     private String password;
-
+    
+    @OneToMany(mappedBy = "usuario")
+    private List<Producto> productos;
+    
+    @OneToMany(mappedBy = "usuario")
+    private List<Orden> ordenes;
+    
     public Usuario() {
     }
 
-    public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono, String tipo, String password) {
+    public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono, String tipo, String password, List<Producto> productos, List<Orden> ordenes) {
         this.id = id;
         this.nombre = nombre;
         this.username = username;
@@ -24,6 +43,24 @@ public class Usuario {
         this.telefono = telefono;
         this.tipo = tipo;
         this.password = password;
+        this.productos = productos;
+        this.ordenes = ordenes;
+    }
+
+    public List<Orden> getOrdenes() {
+        return ordenes;
+    }
+
+    public void setOrdenes(List<Orden> ordenes) {
+        this.ordenes = ordenes;
+    }
+ 
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
     }
     
     public Integer getId() {
