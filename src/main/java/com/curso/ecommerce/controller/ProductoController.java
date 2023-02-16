b7ac0d2762834b8bd87d1fc6c0e381d5b5d29f21
EliasGiova <eliasgiooovanella@gmail.com>
2023-02-15 23:21:09 -0300

    implementacion para cargar imagenes

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -4,13 +4,21 @@ package com.curso.ecommerce.controller;
 
 
 import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.service.ProductoService;
+import com.curso.ecommerce.service.UploadFileService;
+import java.io.IOException;
+import java.util.Optional;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/productos")
@@ -18,24 +26,78 @@ public class ProductoController {
     
     private final Logger logger = LoggerFactory.getLogger(ProductoController.class);//crea mensajes para el seguimiento o
                                                                                     //registro de la ejecución de una aplicación.
+    
     @Autowired
     private ProductoService productoService;
     
+    @Autowired
+    private UploadFileService upload;
+    
+    //muestra la interface de la vista de los productos
     @GetMapping("")
-    public String show(){
+    public String show(Model model){//objeto de tipo model lleva informacion desde el back-end hacia la vista
+        model.addAttribute("productos", productoService.findAll());
         return "productos/show";
     }
     
+    //recibe los atributos para crear el producto
     @GetMapping("/create")
     public String create(){
         return "productos/create";
     }
     
+    //guarda el producto y redirecciona a la lista con el producto para mostrarlo por pantalla
     @PostMapping("/save")
-    public String save(Producto producto){
+    public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException{
         logger.info("Este es el objeto producto{}", producto);
+        Usuario u = new Usuario(1, "", "", "", "", "", "", "");
+        producto.setUsuario(u);
+        
+        //imagen
+        if (producto.getId()==null) {//esta validacion es cuando se crea un producto
+            String nombreImagen = upload.saveImage(file);
+            producto.setImagen(nombreImagen);
+        }else{
+            if (file.isEmpty()) {// editamos el producto pero no cambiamos la imagen
+                Producto p = new Producto();
+                p = productoService.get(producto.getId()).get();
+                producto.setImagen(p.getImagen());
+            }else{
+                String nombreImagen = upload.saveImage(file);
+                producto.setImagen(nombreImagen);
+            }
+        }
+        
         productoService.save(producto);
         return "redirect:/productos";
     }
+    
+    //recibe para modificar
+    @GetMapping("/edit/{id}")//se pone entre llave para recibir el id para poder modificarlo
+    public String edit(@PathVariable Integer id, Model model){//se declara PathVariable para mapear la id que viene en la url para guardarla en la id 
+       Producto producto = new Producto();
+       Optional<Producto> optionalProducto=productoService.get(id);
+       producto=optionalProducto.get();
+       
+       logger.info("Producto Buscado: {}", producto);
+       model.addAttribute("producto", producto);
+       
+       return "productos/edit";
+    }
+    
+    //realiza las modificaciones y las manda a la vista
+    @PostMapping("/update")
+    public String update(Producto producto){
+        productoService.update(producto);
+        return "redirect:/productos";
+    }
+    
+    //recibe el id que quiere eliminar y luego redirecciona a la lista sin el producto eliminado
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id){
+        productoService.delete(id);
+        return "redirect:/productos";
+    }
+    
     
 }
--- src/main/java/com/curso/ecommerce/model/Usuario.java
+++ src/main/java/com/curso/ecommerce/model/Usuario.java
@@ -34,7 +34,7 @@ public class Usuario {
     public Usuario() {
     }
 
-    public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono, String tipo, String password, List<Producto> productos, List<Orden> ordenes) {
+    public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono, String tipo, String password) {
         this.id = id;
         this.nombre = nombre;
         this.username = username;
@@ -43,8 +43,6 @@ public class Usuario {
         this.telefono = telefono;
         this.tipo = tipo;
         this.password = password;
-        this.productos = productos;
-        this.ordenes = ordenes;
     }
 
     public List<Orden> getOrdenes() {
