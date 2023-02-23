301b1a39a6af2ce22121518dfd5a212a6c02373b
EliasGiova <eliasgiooovanella@gmail.com>
2023-02-22 22:10:35 -0300

    borramos todo de spring security

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.*;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,7 +29,7 @@ public class UsuarioController {
     @Autowired
     private IOrdenService ordenService;
     
-    BCryptPasswordEncoder passEncode = new BCryptPasswordEncoder();
+    
 
     /////////////////////////////////////////////////////////////////////
     
@@ -44,7 +43,7 @@ public class UsuarioController {
     public String save(Usuario usuario) {
         logger.info("el usuario llego perfectamente {}", usuario);
         usuario.setTipo("USER");
-        usuario.setPassword(passEncode.encode(usuario.getPassword()));
+        //usuario.setPassword(passEncode.encode(usuario.getPassword()));
         usuarioService.save(usuario);
         return "redirect:/";
     }
